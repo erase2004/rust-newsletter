7d86d9ff9503191a2aa3547f4379508a023a77ea
Tsuki Akiba <erase2004@gmail.com>
2025-09-09 18:54:04 +0800

    feat: add basic subscription function

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use newsletter::configuration::get_configuration;
+use newsletter::startup::run;
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    println!("The server is running on {address}");
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
+}
