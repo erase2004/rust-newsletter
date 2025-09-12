a2c289dcb4c93babb92d3c2edf5537b9494189b5
Tsuki Akiba <erase2004@gmail.com>
2025-09-12 16:13:27 +0800

    refactor: reorganize api tests

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
 use newsletter::configuration::get_configuration;
-use newsletter::startup::run;
+use newsletter::startup::Application;
 use newsletter::telemetry::{get_subscriber, init_subscriber};
-use sqlx::PgPool;
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy_with(configuration.database.connect_options());
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    println!("The server is running on {address}");
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
