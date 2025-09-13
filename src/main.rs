b155a6e8075dfb30b50174830df01eddfc9b36f8
Tsuki Akiba <erase2004@gmail.com>
2025-09-13 13:52:55 +0800

    feat: add subscription confirmation

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use newsletter::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    let subscriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let application = Application::build(configuration).await?;
